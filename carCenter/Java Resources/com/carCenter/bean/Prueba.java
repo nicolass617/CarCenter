673f84b70a13a3eb0f8a427a598fdbb240e899ad
nicolass617 <nicolas.jav@hotmail.com>
2022-05-18 22:52:16 -0500

    cargar inicial

---
+++ carCenter/Java Resources/com/carCenter/bean/Prueba.java
@@ -0,0 +1,12 @@
+package com.carCenter.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean(name = "prueba")
+@ViewScoped
+public class Prueba {
+	
+	
+
+}
